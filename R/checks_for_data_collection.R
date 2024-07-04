8a8a828fa3d91c66d4504bc9cd97c1c7be2c2ef9
DESKTOP-5G8AC30\Acted Laptop <amos.nyarwaya@gmail.com>
2024-07-04 09:44:01 +0300

    start data collection tracker

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -5,3 +5,8 @@ library(supporteR)
 add_dot_gitkeep_to_folder(
   input_sub_folders = c("inputs", "outputs", "support_files")
 )
+add_gitignore_entries(
+  input_lines_to_add = c("\n# Add file types to ignore", "*.html", "*.xlsx", "*.xls",
+                         ".csv", ".pdf", ".doc", ".docx", "inputs/*", "outputs/*", "support_files/*",
+                         "!/**/.gitkeep")
+)
