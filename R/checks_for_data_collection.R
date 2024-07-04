f1678aa15f00b02e7dcef4b088ac08fa41ce7a6d
DESKTOP-5G8AC30\Acted Laptop <amos.nyarwaya@gmail.com>
2024-07-04 08:13:04 +0300

    initial commit

---
+++ R/checks_for_data_collection.R
@@ -0,0 +1,7 @@
+library(supporteR)
+ create_project_sub_folders(
+   input_sub_folders = c("inputs", "outputs", "R", "support_files")
+ )
+add_dot_gitkeep_to_folder(
+  input_sub_folders = c("inputs", "outputs", "support_files")
+)
